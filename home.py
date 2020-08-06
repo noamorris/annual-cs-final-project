b7a217e6ac7149aa72dbbb5c646a4d4dc4beee0b
noa <noa.ariel.morris@gmail.com>
2020-08-06 13:15:34 -0400

    Create home.py

---
+++ home.py
@@ -0,0 +1,76 @@
+<html>
+<head>
+<style>
+body{
+	background-color: #f2c0c0;
+	text-align: center;
+	align-items: center;
+justify-content: center;
+}
+h1{
+	color: #a3445d;
+	font-size: 60px;
+	text-shadow: 3px 2px #7e3548;
+}
+body{
+	color: #8d2d56;
+}
+.button{
+	background-color: rgba(213, 60, 60, 1);
+	font-size: 20px;
+	padding: 10px 28px;
+	border-radius: 6px;
+	-webkit-transition-duration: 0.4s; 
+  	transition-duration: 0.6s;
+	box-shadow: 0 10px 18px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+	display: inline-block;
+	margin: 5px;
+	color: black;
+	text-decoration: none;
+	text-align: center;
+}
+.button:hover{
+	background-color: rgba(255, 255, 255, 0.35);
+	border: 5px solid rgba(213, 60, 60, 1);
+	font-size: 22px;
+	padding: 25px 28px;
+}
+.button:active {
+	box-shadow: 0 5px #666;
+	transform: translateY(4px);
+}
+#buttons {
+	margin-left:auto;
+	margin-right:auto;
+	width: 13%
+
+}
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background-color: #a3445d;
+  color: #f2c0c0;
+  text-align: center;
+}
+</style>
+<title>welcome</title>
+<link rel="icon" href="https://yokoent.com/images250_/star-1.png">
+</head>
+<body>
+<center>
+<br>
+<h1> welcome </h1>
+<h2> choose a quiz or activity</h2>
+<div id="buttons">
+	<a href= "zodiac.html" class="button" style="float:left;">zodiac<br> signs <br> <img src='https://images.britcdn.com/wp-content/uploads/2017/08/zodiac_circle.jpg?w=1000&auto=format' style="width:100px;height:100px;"></a>
+	<a href= "color.html" class="button" style="float:left;">what color are you?</a>
+	<a href= "shoe.html" class="button" style="float:left;">what shoe are you?</a> 
+</div>
+</center>
+<div class='footer'>
+  <p>by noa morris</p>
+</div>
+</body>
+</html>
